bc40589dc51b013e37f37e6c728739327232ddd2
Untoldwind <landru@untoldwind.net>
2015-08-13 23:42:23 +0200

    Editable experiment

--- ui/src/components/EditorAssembler.js
+++ ui/src/components/EditorAssembler.js
@@ -4,9 +4,31 @@ export default class EditorAssembler extends React.Component {
     render() {
         var data = '1\n2\n3\n4\n';
         return (
-            <pre>{data}
+            <pre onInput={this.emitChange.bind(this)}
+                 onBlur={this.emitChange.bind(this)}
+                 contentEditable="true"
+                 dangerouslySetInnerHTML={{__html: data}}>
             </pre>
         );
+    }
+
+    shouldComponentUpdate(nextProps) {
+        return nextProps.data !== React.findDOMNode(this).innerHTML;
+    }
+
+    componentDidUpdate() {
+        if ( this.props.html !== React.findDOMNode(this).innerHTML ) {
+            React.findDOMNode(this).innerHTML = this.props.data;
+        }
+    }
+
+    emitChange(evt) {
+        var data = React.findDOMNode(this).innerHTML;
+        if (this.props.onChange && data !== this.lastData) {
+            evt.target = { value: data };
+            this.props.onChange(evt);
+        }
+        this.lastData = data;
     }
 }
 
